333cf299b1777aeeec6286eed1c41e3679024921
Jimmy Bowens <jimmy@jimmybowens.com>
2014-11-26 08:09:31 -0500

    initial commit

---
+++ Java Database Project/src/java_database_project/TabbedPanel.java
@@ -0,0 +1,12 @@
+package java_database_project;
+
+import javax.swing.JPanel;
+
+public class TabbedPanel extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+
+	public TabbedPanel (){
+		
+	}
+}
